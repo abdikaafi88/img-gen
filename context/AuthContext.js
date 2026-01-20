446527116a8624867f64e2be9744f3af4188bdd9
suhaybcabdicasiis <suhaybcabdicasiis14@gmail.com>
2026-01-20 12:13:51 +0300

    Update AuthContext.js

--- context/AuthContext.js
+++ context/AuthContext.js
@@ -12,6 +12,7 @@ export const AuthProvider = ({ children }) => {
   const [user, setUser] = useState(null);
   const [loading, setLoading] = useState(true);
   const [token, setToken] = useState(null);
+  
 
   useEffect(() => {
     loadUser();
